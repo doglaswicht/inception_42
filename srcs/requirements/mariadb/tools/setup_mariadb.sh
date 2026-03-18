cbf33a8e89d2126ef643be0a23f50a40afbeef19
dw <doglasbastos@hotmail.com>
2026-03-19 00:14:54 +0100

    dockerfile mariadb

---
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 2
+
+mysql -u root << EOF
+
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+FLUSH PRIVILEGES;
+
+EOF
+
+service mariadb stop
+
+# Executa o MariaDB em foreground (OBRIGATÓRIO)
+exec mysqld_safe
